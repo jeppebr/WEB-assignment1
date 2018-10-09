bff295870402460dcd30de2fa1319460a8d78934
Christian Maigaard Christensen <>
2018-10-09 13:51:12 +0200

    login button now calls angular function

--- app_api/controllers/login.js
+++ app_api/controllers/login.js
@@ -1,6 +1,9 @@
 
 module.exports.loginCreate = function(req, res) { 
-    res
-    .status(200)
-    .json({"message" : "Login request received"});
+    console.log(req.url)
+    console.log("hello")
+    res.redirect('http://localhost:4200/')
+    // res
+    // .status(200)
+    // .json({"message" : "Login request received"});
 };
--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+
+  sendLoginInfo() {
+    console.log("sendLoginInfo")
+
+    // var body = 'username=myusername&password=mypassword';
+    // var headers = new Headers();
+    // headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    // this.HttpClient
+    //   .post('/api',
+    //     body, {
+    //       headers: headers
+    //     })
+    //     .subscribe(data => {
+    //           alert('ok');
+    //     }, error => {
+    //         console.log(JSON.stringify(error.json()));
+    //     });
+}
+
+  
 
   constructor() { }
 
